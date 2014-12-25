b2eb29fe203cb4b1dabd22903a1cb8e3dbe0d090
Ben <ben@concutere.com>
2014-12-24 18:00:08 -0800

    modified opentype.js for svg path rendering
    rendering path in svg allows for better event handling. need to refactor
    opentype.js changes before pull request to its master.

--- sigil.js
+++ sigil.js
@@ -1,8 +1,12 @@
 (function() {//TODO encapsulate ...
+  var svgNS = "http://www.w3.org/2000/svg";
   var gf; 
   var cvs = document.getElementById('canvas');
   var ctx = cvs.getContext('2d');
   var svg = document.getElementById('svg');
+  var gPath = document.getElementById('gPath');
+  var gCtls = document.getElementById('gCtls');
+
   opentype.load('fonts/Roboto-Black.ttf', function (err, font) {
       if (err) {
            alert('Font could not be loaded: ' + err);
@@ -15,7 +19,7 @@
           var path = font.getPath('TYPE HERE', 25, 150, 72);
           // If you just want to draw the text you can also use font.draw(ctx, text, x, y, fontSize).
           path.fill='CRIMSON';
-          path.draw(ctx);
+          path.draw(gPath);
       }
   });
 
@@ -23,9 +27,11 @@
   var chars=[];
   var cPaths=[]; //TODO use for animating transition from user's statement text into set of letters
   var showpath;
+  var showCtls = true;
+
   document.addEventListener('keydown',handleCmd);
   function handleCmd(e) {
-    if (e.which === 8) {
+    if (e.keyCode === 8) {
       e.preventDefault();
       //DELETE CHAR
       chars.pop();
@@ -33,12 +39,16 @@
       drawTypedText();
       return false;
     }
+    if (e.keyCode === 27) {
+      showCtls = !showCtls;
+      reshowCtls();
+    }
   }
 
   document.addEventListener('keypress',update);
   function update(e) {
     if (text=="") {
-      clear();
+      clearPath();
     }
     else if (e.keyCode == 13) { //enter key
       setKeys(text);
@@ -59,15 +69,22 @@
   function drawTypedText() {
     var path = gf.getPath(text,25,200,36);
     path.fill='crimson';
-    clear();
-    path.draw(ctx);
+    clearPath();
+    path.draw(gPath);
   }
 
   function clear() {
     ctx.clearRect(0,0,cvs.width, cvs.height);
   }
+
+  function clearPath() {
+    clearsvg(gPath);
+  }
+  function clearCtls() {
+    clearsvg(gCtls);
+  }
   
-  function clearsvg() {
+  function clearsvg(svg) {
     while(svg.lastChild) {
       svg.removeChild(svg.lastChild);
     }
@@ -76,19 +93,39 @@
   function setKeys(text,skipCtls) {
     document.removeEventListener('keypress',update);
     
-    clear();
+    clearPath();
     
     var path = gf.getPath(chars.join(''),25,100,88);
     path.fill='crimson';
-    path.draw(ctx);
+    path.draw(gPath);
     showpath=path;
     //setup edit els
     if(!skipCtls) {
-      clearsvg();
+      clearCtls();
       drawCtls(path.commands);
     }
-    
-    //TODO esc (or ?) to toggle ctl visibility
+
+    gPath.addEventListener('dblclick',editPathColor);
+
+    svg.addEventListener('dblclick',function(e) {if(e.target.constructor.name=="SVGSVGElement") { showCtls = !showCtls; reshowCtls(); }});
+  }
+
+  function editPathColor(e) {
+    if (e.target.getAttribute('fill')=='crimson') {
+      e.target.setAttribute('fill', 'forestgreen');
+    }
+    else {
+      e.target.setAttribute('fill', 'crimson');
+    }
+  }
+
+  function reshowCtls() {
+    //actually just toggles visibility ...
+    var ctls = document.getElementsByClassName('ctl');
+    for(var i=0; i <ctls.length; i++) {
+      var ctl = ctls[i];
+      ctl.style.visibility = showCtls ? 'visible' : 'hidden';
+    }
   }
 
   function drawCtls(cmds) {
@@ -115,12 +152,12 @@
   function drawCtl(id,cmd, x, y) {
     var isCtlPt = parseFloat(id) != Math.floor(id);
     var i = parseInt(Math.floor(id));
-    var svgNS = "http://www.w3.org/2000/svg";
     var el = document.createElementNS(svgNS, "circle");
     el.id='ctl'+id;
     el.r.baseVal.value=isCtlPt ? 3.5 : 4;
     el.cx.baseVal.value = x;
     el.cy.baseVal.value = y;
+    el.setAttribute('class','ctl');
     el.setAttribute(isCtlPt ? 'stroke' : 'fill','azure');
     if (isCtlPt) {
       el.setAttribute('fill','transparent');
@@ -130,23 +167,23 @@
       el.setAttribute('fill-opacity', '0.5');
     }
 
-    svg.appendChild(el);
+    gCtls.appendChild(el);
     el.addEventListener('mousedown',mousedown);
-    el.addEventListener('mouseup',mouseup); //TODO handle like mousemove
-    el.addEventListener('dblclick',dblclick);
+    el.addEventListener('mouseup',mouseup); //TODO handle like mousemove (?)
+    el.addEventListener('dblclick',delEl);
     return el;
   }
   
-  function dblclick(e) {
+  function delEl(e) {
     var id = e.target.id.substr(3);
     if (!isNaN(id) && id > -1) {
       var i = parseInt(Math.floor(id));
       var cmd = showpath.commands[i];
       showpath.commands.splice(i,1);
 
-      clear();
-      clearsvg();
-      showpath.draw(ctx);
+      clearPath();
+      clearCtls();
+      showpath.draw(gPath);
       drawCtls(showpath.commands);
     }
   }
@@ -192,8 +229,8 @@
       showpath.commands[i].y = dragel.cy.baseVal.value;
     }
     {dragel=undefined;}    
-    clear();
-    showpath.draw(ctx);
+    clearPath();
+    showpath.draw(gPath);
     
   }
 })();
